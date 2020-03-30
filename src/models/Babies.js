40fd454f5736edd7b7dd9b633e7c3fad9dff1c38
Stiven Trujillo <strujillo1293@gmail.com>
2020-03-30 00:20:36 -0500

    Models

---
+++ src/models/Babies.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const BabySchema = new Schema({
+    first_name: {type: String, required: true},
+    last_name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    cellphone: {type: String, required: true, unique: true},
+    birth_date: Date,
+    profile_pic: String,
+    score: {type: Number, enum: [1, 2, 3, 4, 5]},
+    gender: {type: String, enum: ['M', 'F', 'O']},
+    liked_by: {type: [Schema.Types.ObjectId], ref: 'posts'},
+    is_active: {type: Boolean, default: true},
+}, { timestamps: true });
+
+module.exports = mongoose.model('babies', BabySchema);
