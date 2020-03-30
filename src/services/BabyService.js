6c725ed0b6f159c00b936ae4cf692a0a330d6c78
Stiven Trujillo <strujillo1293@gmail.com>
2020-03-30 00:38:42 -0500

    services basic structure

---
+++ src/services/BabyService.js
@@ -0,0 +1,19 @@
+const Babies = require('../models/Babies');
+
+const createOneBaby = () => {};
+
+const readAllBabies = () => {};
+
+const readOneBaby = () => {};
+
+const updateOneBaby = () => {};
+
+const deleteOneBaby = () => {};
+
+module.exports = {
+    createOneBaby,
+    readAllBabies,
+    readOneBaby,
+    updateOneBaby,
+    deleteOneBaby
+};
---
+++ src/services/ParentService.js
@@ -0,0 +1,19 @@
+const Parents = require('../models/Parents');
+
+const createOneParent = () => {};
+
+const readAllParents = () => {};
+
+const readOneParent = () => {};
+
+const updateOneParent = () => {};
+
+const deleteOneParent = () => {};
+
+module.exports = {
+    createOneParent,
+    readAllParents,
+    readOneParent,
+    updateOneParent,
+    deleteOneParent
+};
