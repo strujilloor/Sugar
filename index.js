3260c485702057e9e061cec4d31878003a0af607
Stiven Trujillo <strujillo1293@gmail.com>
2020-04-12 21:06:11 -0500

    Part 6 Server

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 const { GraphQLServer } = require('graphql-yoga');
 const { importSchema } = require('graphql-import');
+const { makeExecutableSchema } = require('graphql-tools');
+const verifyToken = require('./src/utils/verifyToken'); 
+const AuthDirective = require('./src/resolvers/Directives/AuthResolver');
 const resolvers = require('./src/resolvers');
 const mongoose = require('mongoose');
+
+const port = process.env.PORT || 4000;
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -17,9 +22,22 @@ mongo.on('error', error => console.log(error))
 
 const typeDefs = importSchema( __dirname + '/schema.graphql');
 
-const server = new GraphQLServer({
+const schema = makeExecutableSchema({
     typeDefs,
-    resolvers
+    resolvers,
+    schemaDirectives: {
+        auth: AuthDirective,
+    }
 });
 
-server.start(() => console.log('Servidor arriba en puerto 4000'));
+const server = new GraphQLServer({
+    schema,
+    context: async (contextParams) => ({
+        ...contextParams,
+        userAuth: contextParams.request // aquí viene nuestras cabeceras, nuestro body, nuestros params (en un axios trae el objeto request)
+            ? await verifyToken(contextParams.request) // si si vienen, verificamos nuestro token, y le mandamos el request
+            : {}, // si no viene voy a guardar un objeto vacio en el contexto
+    }),
+});
+
+server.start( {port}, ( ) => console.log(`Servidor arriba en puerto ${ port }`)) ;
